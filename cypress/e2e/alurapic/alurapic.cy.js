54389ef6290b9984b5d22b57f71b513745641464
luh <luisa.santos@db.tec.br>
2023-02-09 05:45:59 -0300

    Finalizando testes de registro

--- cypress/e2e/alurapic/alurapic.cy.js
+++ cypress/e2e/alurapic/alurapic.cy.js
@@ -1,17 +1,47 @@
-describe('Login e registro de usuarios alura pic', () => {
+describe('Registro de usuarios alura pic', () => {
 
     beforeEach(() => {
-      cy.visit('https://alura-fotos.herokuapp.com')
+      cy.visit('https://alura-fotos.herokuapp.com');
+      cy.contains('a', 'Register now').click();
 
      })
 
     it('verifica mensagens validacao', () => {
-        cy.contains('a', 'Register now').click();
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+    })
+
+    it('verifica mensagem de email invalido', () => {
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('Luisa');
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+    })
+
+    it('verifica mensagem de senha com menos de 8 caracteres', () => {
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+    })
+
+    it('verifica mensagem de nome completo com menos de 2 caracteres', () => {
+        cy.get('input[formcontrolname="userName"]').type('1');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
+    })
+    
+    it('verifica mensagem de nome de usuario com menos de 2 caracteres', () => {
+        cy.get('input[formcontrolname="userName"]').type('1');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
+    })
+
+    it('verifica mensagem de nome de usuario em maiúsculo', () => {
+        cy.get('input[formcontrolname="userName"]').type('ABC');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
     })
 })
